39f9f9c8fc00fa03f309edc6fd9896749adce5c8
Mark <mranda10@my.centennialcollege.ca>
2021-11-05 10:38:32 -0400

    Initial Commit

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = 
+{
+    //"URI": "mongodb://localhost/business"
+    "URI": "mongodb+srv://markmidterm:y4sZ7J7fwUV3gFlJ@midterm-bookcollection.t0pii.mongodb.net/Angular?retryWrites=true&w=majority",
+    "Secret": 'SomeSecret'
+}
